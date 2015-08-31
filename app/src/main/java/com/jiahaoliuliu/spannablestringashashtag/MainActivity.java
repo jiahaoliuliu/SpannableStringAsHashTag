1a9c9a541b21be692cb385a954dcb7db7c0ac791
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-08-31 22:51:03 +0400

    Added simple text to test

--- app/src/main/java/com/jiahaoliuliu/spannablestringashashtag/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/spannablestringashashtag/MainActivity.java
@@ -1,17 +1,20 @@
 package com.jiahaoliuliu.spannablestringashashtag;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mHashTagsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHashTagsTextView = (TextView) findViewById(R.id.hash_tags_text_view);
+
+        mHashTagsTextView.setText(getString(R.string.hash_tags_text));
     }
 }
